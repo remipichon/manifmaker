cfa0b642d869f33dbae4aef844598b7f487271d3
remipichon <pichon.remi.pr@gmail.com>
2015-12-04 23:09:07 +0100

    [RPI] add materialize + left menu

---
+++ manifmaker/client/helpers-events/common/menu.js
@@ -0,0 +1,5 @@
+
+Template.wrapper.rendered = function(){
+    $("#button-collapse-left-menu").sideNav();
+    $('.collapsible-left-menu').collapsible();
+};
--- manifmaker/client/route.js
+++ manifmaker/client/route.js
@@ -1,16 +1,36 @@
+Router.configure({
+    layoutTemplate: 'wrapper'
+});
+
+//hide menu for each new route
+Router.onAfterAction(function () {
+    $('#button-collapse-left-menu').sideNav('hide');
+});
+
 
 Router.route('/tasks', function () {
-    this.render('TasksList');
-});
+        this.render('TasksList');
+    },
+    {name: 'task.list'}
+);
 
 Router.route('/activities', function () {
-    this.render('ActivitiesList');
-});
+        this.render('ActivitiesList');
+    },
+    {name: 'activity.list'}
+);
 
 Router.route('/assignment', function () {
-    this.render('AssignmentHome');
-});
+        this.render('AssignmentHome', {to: 'mainContent'});
+    },
+    {name: 'assignment.calendar'}
+);
+
 
 Router.route('/', function () {
-    this.render('home');
-});
+        this.render('home', {to: 'mainContent'});
+    },
+    {name: 'home'}
+);
+
+
