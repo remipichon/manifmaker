f6b5b59c0dc93ad829597bed47949d07beb4930e
remipichon <pichon.remi.pr@gmail.com>
2015-12-04 23:23:46 +0100

    [RPI] clean some no longer needed buttons

--- manifmaker/client/helpers-events/place/place.js
+++ manifmaker/client/helpers-events/place/place.js
@@ -5,19 +5,15 @@ Template.places.helpers({
 });
 
 Template.places.events({
-    "click button[name=newPlaceButton]" : function(event){
+    "click .save" : function(event){
         event.preventDefault();
         var placeName = $('input[name=placeName]').val();
         var place = new Place(placeName);
 
         if(this._id){ //already exist, we update it
-            Places.update({_id: this._id}, place, function(error,results){
-                Router.update('place');
-            });
+            Places.update({_id: this._id}, place);
         } else { //doesn't already exist, we create it
-            Places.insert(place, function(error,results){
-                Router.update('place');
-            });
+            Places.insert(place);
         }
     }
 });
