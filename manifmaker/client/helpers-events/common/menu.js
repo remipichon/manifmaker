076820f94df3b8430c8cef9b7a498345c4221b30
remipichon <pichon.remi.pr@gmail.com>
2015-12-06 17:37:42 +0100

    [RPI] design userList and taskList for assignment

--- manifmaker/client/helpers-events/common/menu.js
+++ manifmaker/client/helpers-events/common/menu.js
@@ -2,4 +2,9 @@
 Template.wrapper.rendered = function(){
     $("#button-collapse-left-menu").sideNav();
     $('.collapsible-left-menu').collapsible();
+
+    $('.collapsible').collapsible({
+        accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+
 };
