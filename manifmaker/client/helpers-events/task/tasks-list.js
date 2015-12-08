d49bd29597725dce9fdb632f4640aed8602a7f55
ConstantPALLIERE <constant.palliere@gmail.com>
2015-12-08 15:36:01 +0100

    [CP]Affichage du nom des équipes

--- manifmaker/client/helpers-events/task/tasks-list.js
+++ manifmaker/client/helpers-events/task/tasks-list.js
@@ -7,7 +7,7 @@ Template.tasksList.helpers({
             multiColumnSort: true,
             fields: [{label: 'Groupe', fnAdjustColumnSizing:true},
                 {key: 'name', label: 'Nom de la tache', fnAdjustColumnSizing:true},
-                {key: 'team', label: 'Equipe', fnAdjustColumnSizing:true},
+                {key: 'team.name', label: 'Equipe', fnAdjustColumnSizing:true},
                 {key: 'timeSlots', label: 'Nombre de créneaux', sortable: false, fn: function (timeSlots, Task) {
                     return timeSlots.length;
                     }, fnAdjustColumnSizing:true}
