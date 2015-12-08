222baab32fd499815826590970ffd1958d5f15fa
Remi Pichon <pichonremipr@gmail.com>
2015-12-08 09:39:27 +0100

    [RPI] code runs now

--- manifmaker/client/helpers-events/task/tasks-list.js
+++ manifmaker/client/helpers-events/task/tasks-list.js
@@ -4,13 +4,17 @@ Template.tasksList.helpers({
             collection: Tasks,
             rowsPerPage: 10,
             showFilter: true,
-            multiColumnSort:true,
-            fields: [{ key: 'name', label: 'Nom de la tache' },
-                {  label: 'Groupe' },
-                { key: 'team', label: 'Equipe' },
-                { key: 'timeSlots', label:'Nombre de créneaux', fn:function(timeSlots,Task){return timeSlots.length;}}]
+            multiColumnSort: true,
+            fields: [{key: 'name', label: 'Nom de la tache'},
+                {label: 'Groupe'},
+                {key: 'team', label: 'Equipe'},
+                {
+                    key: 'timeSlots', label: 'Nombre de créneaux', fn: function (timeSlots, Task) {
+                    return timeSlots.length;
+                }
+                }]
         };
     },
+});
 
 Template.tasksList.events({});
-Template.tasksList.events({
