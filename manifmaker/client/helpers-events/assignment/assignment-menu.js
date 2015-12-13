40045ba0242e8a608b80b85aee6a5a96fd32c6ad
remipichon <pichon.remi.pr@gmail.com>
2015-12-13 20:01:56 +0100

    [RPI] restore assignment breadcrumb

--- manifmaker/client/helpers-events/assignment/assignment-menu.js
+++ manifmaker/client/helpers-events/assignment/assignment-menu.js
@@ -33,99 +33,99 @@ Template.assignmentMenu.helpers({
         }
         return "";
 
+    },
+    breadCrumbAssignment: function () {
+        var userFilter = UserFilter.get(),
+            taskFilter = TaskFilter.get(),
+            currentAssignmentType = CurrentAssignmentType.get(),
+            selectedUser = SelectedUser.get(),
+            selectedTask = SelectedTask.get(),
+            selectedDate = SelectedDate.get(),
+            selectedTimeSlot = SelectedTimeSlot.get(),
+            result = [];
+
+        if (currentAssignmentType === AssignmentType.USERTOTASK) {
+            if (selectedUser === null) {
+                result.push({
+                    label: "Select a user",
+                    url: ""
+                });
+            } else  {
+                var userName = UserRepository.findOne(selectedUser._id).name;
+                result.push({
+                    label: userName,
+                    url: "/assignment/userToTask/" + selectedUser._id
+                });
+
+                if (selectedAvailability === null) {//TODO pas top
+                    result.push({
+                        label: "Select one of the availability",
+                        url: ""
+                    });
+                } else {
+                    result.push({
+                        label: selectedDate.format("ddd D HH:mm"),
+                        url: "/assignment/userToTask/" + selectedUser._id + "/" + selectedDate.format('x')
+                    });
+
+                    if(true){ //if pas de task/people need selected
+                        result.push({
+                            label: "Select one of the available task [and a people need]",
+                            url: ""
+                        });
+                    }
+                }
+            }
+            return result;
+        }
+
+        if (currentAssignmentType === AssignmentType.TASKTOUSER) {
+            if (selectedTask === null) {
+                result.push({
+                    label: "Select a task",
+                    url: ""
+                });
+            } else  {
+                var taskName = TaskRepository.findOne(selectedTask._id).name;
+                result.push({
+                    label: taskName,
+                    url: "/assignment/taskToUser/" + selectedTask._id
+                });
+
+                if (selectedTimeSlot === null) {//TODO pas top
+                    result.push({
+                        label: "Select one of the time slot [and a people need]",
+                        url: ""
+                    });
+                } else {
+
+                    var task = Tasks.findOne(selectedTask);
+                    var timeSlot = TimeSlotService.getTimeSlot(task,selectedTimeSlot._id);
+
+
+                    result.push({
+                        label: new moment(timeSlot.start).format("HH:mm") + " to " +  new moment(timeSlot.end).format("HH:mm"),
+                        url: "/assignment/taskToUser/" + selectedTask._id + "/" + selectedTimeSlot._id
+                    });
+
+                    if(true){ //if pas de task/people need selected
+                        result.push({
+                            label: "Select one of the available user",
+                            url: ""
+                        });
+                    }
+                }
+            }
+            return result;
+        }
+
+        if(currentAssignmentType == AssignmentType.ALL){
+            result.push({
+                label: "Welcome, start by selecting an assignment mode",
+                url: ""
+            });
+            return result;
+        }
+
     }
-//    breadCrumbAssignment: function () {
-//        var userFilter = UserFilter.get(),
-//            taskFilter = TaskFilter.get(),
-//            currentAssignmentType = CurrentAssignmentType.get(),
-//            selectedUser = SelectedUser.get(),
-//            selectedTask = SelectedTask.get(),
-//            selectedDate = SelectedDate.get(),
-//            selectedTimeSlot = SelectedTimeSlot.get(),
-//            result = [];
-//
-//        if (currentAssignmentType === AssignmentType.USERTOTASK) {
-//            if (selectedUser === null) {
-//                result.push({
-//                    label: "Select a user",
-//                    url: ""
-//                });
-//            } else  {
-//                var userName = UserRepository.findOne(selectedUser._id).name;
-//                result.push({
-//                    label: userName,
-//                    url: "/assignment/userToTask/" + selectedUser._id
-//                });
-//
-//                if (selectedAvailability === null) {//TODO pas top
-//                    result.push({
-//                        label: "Select one of the availability",
-//                        url: ""
-//                    });
-//                } else {
-//                    result.push({
-//                        label: selectedDate.format("ddd D HH:mm"),
-//                        url: "/assignment/userToTask/" + selectedUser._id + "/" + selectedDate.format('x')
-//                    });
-//
-//                    if(true){ //if pas de task/people need selected
-//                        result.push({
-//                            label: "Select one of the available task [and a people need]",
-//                            url: ""
-//                        });
-//                    }
-//                }
-//            }
-//            return result;
-//        }
-//
-//        if (currentAssignmentType === AssignmentType.TASKTOUSER) {
-//            if (selectedTask === null) {
-//                result.push({
-//                    label: "Select a task",
-//                    url: ""
-//                });
-//            } else  {
-//                var taskName = TaskRepository.findOne(selectedTask._id).name;
-//                result.push({
-//                    label: taskName,
-//                    url: "/assignment/taskToUser/" + selectedTask._id
-//                });
-//
-//                if (selectedTimeSlot === null) {//TODO pas top
-//                    result.push({
-//                        label: "Select one of the time slot [and a people need]",
-//                        url: ""
-//                    });
-//                } else {
-//
-//                    var task = Tasks.findOne(selectedTask);
-//                    var timeSlot = TimeSlotService.getTimeSlot(task,selectedTimeSlot._id);
-//
-//
-//                    result.push({
-//                        label: new moment(timeSlot.start).format("HH:mm") + " to " +  new moment(timeSlot.end).format("HH:mm"),
-//                        url: "/assignment/taskToUser/" + selectedTask._id + "/" + selectedTimeSlot._id
-//                    });
-//
-//                    if(true){ //if pas de task/people need selected
-//                        result.push({
-//                            label: "Select one of the available user",
-//                            url: ""
-//                        });
-//                    }
-//                }
-//            }
-//            return result;
-//        }
-//
-//        if(currentAssignmentType == AssignmentType.ALL){
-//            result.push({
-//                label: "Welcome, start by selecting an assignment mode",
-//                url: ""
-//            });
-//            return result;
-//        }
-//
-//    }
 });
