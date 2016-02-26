acb526324f80b3ad696bb3332cbce704b4d11a68
RémiPichon <pichon.remi.pr@gmail.com>
2016-02-26 19:06:30 +0000

    [RPI] task equipment : displayed by categories

--- manifmaker/client/helpers-events/task/task.js
+++ manifmaker/client/helpers-events/task/task.js
@@ -12,12 +12,30 @@ Template.updateTaskForm.helpers({
         return true;
     },
 
+
+    equipmentsCategories: function() {
+        var categories = EquipmentCategories.find().fetch();
+        //remove categories which have nothing to display for this target
+        var result = [];
+        _.each(categories,function(category){
+            if(Equipments.find({EquipmentCategories_Id: category._id, targetUsage : {$in : [EquipementTargetUsage.BOTH, EquipementTargetUsage.TASK]}})
+                    .fetch().length !== 0){
+                result.push(category);
+            }
+        });
+        return result;
+    },
+    equipments: function(category){
+        return Equipments.find({EquipmentCategories_Id: this._id, targetUsage : {$in : [EquipementTargetUsage.BOTH, EquipementTargetUsage.TASK]}});
+    },
+
+
     autoformNameForQuantity: function() {
         //tout ca parce que Spacebars ne supporte pas @index...
         var currentEditingTaskId = SelectedUpdatedOrReadedTask.get();
         var currentTask = Tasks.findOne(currentEditingTaskId);
         var equipments = Tasks.findOne(currentEditingTaskId).equipments;
-        var index = equipments.indexOf(_.findWhere(equipments,{equipmentId :this.equipmentId}))
+        var index = equipments.indexOf(_.findWhere(equipments,{equipmentId :this._id}))
         return "equipments."+ index +".quantity";
     },
     autoformNameForEquipmentId: function() {
@@ -25,13 +43,7 @@ Template.updateTaskForm.helpers({
         var currentEditingTaskId = SelectedUpdatedOrReadedTask.get();
         var currentTask = Tasks.findOne(currentEditingTaskId);
         var equipments = Tasks.findOne(currentEditingTaskId).equipments;
-        var index = equipments.indexOf(_.findWhere(equipments,{equipmentId :this.equipmentId}))
+        var index = equipments.indexOf(_.findWhere(equipments,{equipmentId :this._id}))
         return "equipments."+ index +".equipmentId";
     },
-
-    equipmentName: function(){
-        return Equipments.findOne(this.equipmentId).name;
-    }
-
-
 });
