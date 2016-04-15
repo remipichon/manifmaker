75cb76e48523e94b81152e33e1543fe07c67b86e
RémiPichon <pichon.remi.pr@gmail.com>
2016-04-15 19:52:38 +0100

    [RPI] EditTimeSlotCalendarComponent, hou pinaize !

--- manifmaker/client/helpers-events/task/EditTimeSlotCalendarComponent.js
+++ manifmaker/client/helpers-events/task/EditTimeSlotCalendarComponent.js
@@ -1,5 +1,25 @@
 import {BaseCalendarComponent} from "../common/BaseCalendarComponent"
 
 class EditTimeSlotCalendarComponent extends BaseCalendarComponent{
+    peopleNeedOnClick() {
+        //to implement
+    }
 
+    peopleNeedAssignedOnClick(event) {
+        //to implement
+    }
+
+    creanOnClick() {
+        //to implement
+    }
+
+    quartHeureOnClick(event) {
+        //to implement
+    }
+
+    timeSlot(date, timeHours, idTask) {
+        //to implement
+    }
 }
+
+EditTimeSlotCalendarComponent.register("EditTimeSlotCalendarComponent");
