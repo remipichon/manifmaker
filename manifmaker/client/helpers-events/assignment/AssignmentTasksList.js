2c91d2f64dacd82cb3e0dbe96f642d1391a18435
RémiPichon <pichon.remi.pr@gmail.com>
2016-05-21 13:05:34 +0100

    [RPI] AssigmentTaskList as a Blaze component

--- manifmaker/client/helpers-events/assignment/AssignmentTasksList.js
+++ manifmaker/client/helpers-events/assignment/AssignmentTasksList.js
@@ -1,5 +1,140 @@
-Template.assignmentTasksList.helpers({
-    tasks: function () {
+class AssignmentTasksList extends BlazeComponent {
+    constructor(parent) {
+        super();
+    }
+
+    events() {
+        return [{
+            "click .href-assignment-task": this.onClickTaskName,
+            "click .task": this.onClickTask,
+            "click li.peopleNeed": this.onClickPeopleNeed,
+            "keyup #search_task_name": this.performSearch,
+            "change .filter_team": this.performFilterTeam,
+            "change #filter_needed_team_task": this.performFilterNeededTeam,
+            "change #filter_skills_task": this.performFilterSkills,
+            "change #display-assigned-task-checkbox": this.switchDisplayAssignedTask
+        }]
+    }
+
+    onRendered() {
+        $('#assignment-task-list-collapsible').collapsible({});
+    }
+
+    template() {
+        return "assignmentTaskList";
+    }
+
+    onClickTaskName(event) {
+        console.info("routing", "/assignment/task/" + this.currentData()._id);
+        Router.go("/assignment/task/" + this.currentData()._id);
+    }
+
+    onClickTask(event) {
+        SelectedTaskBreadCrumb.set(this.currentData());
+    }
+
+    onClickPeopleNeed(event) {
+        event.stopPropagation();
+        var currentAssignmentType = CurrentAssignmentType.get();
+        var isUnassignment = IsUnassignment.get();
+        var target = $(event.target);
+        var _idTask, _idTimeSlot;
+        if (target.hasClass("task"))
+            _idTask = target.data("_id");
+        else
+            _idTask = target.parents(".task").data("_id");
+
+        SelectedPeopleNeed.set(this.currentData());
+
+        var userId = SelectedUser.get()._id;
+
+
+        if (target.hasClass("time-slot"))
+            _idTimeSlot = target.data("_id");
+        else
+            _idTimeSlot = target.parents(".time-slot").data("_id");
+
+        switch (currentAssignmentType) {
+            case AssignmentType.USERTOTASK:
+                if (isUnassignment) {
+                    Meteor.call("removeAssignUserToTaskTimeSlot", SelectedPeopleNeed.get()._id, userId);
+                    IsUnassignment.set(false);
+                } else
+                    Meteor.call("assignUserToTaskTimeSlot", SelectedPeopleNeed.get()._id, userId, function (error, result) {
+                        if (!error) {
+                            //we have to reset the task list this way because once assigned, the task can't propose any peopleNeed for the user as he is
+                            //no longer available but the task can still be theoretically assigned to him because we didn't recompute the TaskFilter job done
+                            //by the event on the calendar.
+                            //we don't need to do that for the mode TASKTOUSER as the user naturally disappears from the list once assigned
+                            TaskFilter.set(noneFilter);
+
+                            //reset workflow
+                            SelectedAvailability.set(null);
+                        }
+                    });
+                break;
+            case AssignmentType.TASKTOUSER:
+                break;
+        }
+
+
+    }
+
+
+    performSearch(event) {
+        var searchInput = $("#search_task_name").val();
+
+        //desactivation de la recherche par URL
+        //console.info("routing", "/assignment/task/search/"+query);
+        //Router.go("/assignment/task/search/"+query);
+
+        if (searchInput === "") {
+            TaskIndexFilter.set(noSearchFilter);
+        } else {
+            TaskIndexFilter.set(searchInput);
+        }
+    }
+
+    performFilterTeam(event) {
+        var _id = $(event.target).val();
+        if (_id === "") {
+            TaskTeamFilter.set(defaultFilter);
+        } else {
+            TaskTeamFilter.set({
+                teamId: _id
+            });
+        }
+    }
+
+    performFilterNeededTeam(event) {
+        var _id = $(event.target).val();
+        if (_id === "") {
+            TaskNeededTeamFilter.set(null);
+        } else if (_id === "noNeededTeam") {
+            TaskNeededTeamFilter.set("noNeededTeam");
+        }
+        else {
+            TaskNeededTeamFilter.set(_id);
+        }
+    }
+
+    performFilterSkills(event) {
+        var _ids = $(event.target).val();
+        if (!_ids) {
+            TaskSkillsFilter.set(null);
+        } else if (_ids[0] === "noSkills") {
+            TaskSkillsFilter.set([]);
+        } else {
+            TaskSkillsFilter.set(_ids);
+        }
+    }
+
+    switchDisplayAssignedTask(event) {
+        DisplayAssignedTask.set($($(event.target)[0]).is(':checked'));
+    }
+
+
+    tasks() {
         var filter = TaskFilter.get();
         var filterIndex = TaskIndexFilter.get();
         var teamFilter = TaskTeamFilter.get();
@@ -10,7 +145,7 @@ Template.assignmentTasksList.helpers({
         var neededTeamFilter = TaskNeededTeamFilter.get();
         var skillsAndNeededTeamFilterForAssigned = {};
 
-        if(displayAssignedTask && assignmentType === AssignmentType.TASKTOUSER){
+        if (displayAssignedTask && assignmentType === AssignmentType.TASKTOUSER) {
             skillsAndNeededTeamFilterForAssigned = {
                 timeSlots: {
                     $elemMatch: {
@@ -58,7 +193,7 @@ Template.assignmentTasksList.helpers({
         var searchResult;
         var filterResult;
 
-        if(displayAssignedTask && assignmentType === AssignmentType.TASKTOUSER) {
+        if (displayAssignedTask && assignmentType === AssignmentType.TASKTOUSER) {
             filterResult = Tasks.find({
                 $and: [
                     filter,
@@ -71,7 +206,7 @@ Template.assignmentTasksList.helpers({
                     }
                 ]
             }, {limit: 20}).fetch();
-        }else {
+        } else {
             filterResult = Tasks.find({
                 $and: [
                     filter,
@@ -83,16 +218,18 @@ Template.assignmentTasksList.helpers({
 
         searchResult = TasksIndex.search(filterIndex, {limit: 20}).fetch();
         return _.intersectionObjects(searchResult, filterResult);
-    },
-    team: function () {
-        return Teams.findOne({_id: this.teamId}).name;
-    },
-    user: function () {
-        return Users.findOne({_id: this.userId}).name;
-    },
+    }
 
-    timeSlotsInfo: function(){
-        var task = this;
+    team() {
+        return Teams.findOne({_id: this.currentData().teamId}).name;
+    }
+
+    user() {
+        return Users.findOne({_id: this.currentData().userId}).name;
+    }
+
+    timeSlotsInfo() {
+        var task = this.currentData();
         var timeSlots = task.timeSlots;
         if (CurrentAssignmentType.get() === AssignmentType.USERTOTASK) {
             var result = [];
@@ -107,17 +244,17 @@ Template.assignmentTasksList.helpers({
             });
 
             var string = " - "
-            _.each(result,function(timeSlot){
-                string += new moment(timeSlot.start).format("H[h]mm")+" - "+new moment(timeSlot.end).format("H[h]mm");
+            _.each(result, function (timeSlot) {
+                string += new moment(timeSlot.start).format("H[h]mm") + " - " + new moment(timeSlot.end).format("H[h]mm");
             });
             return string;
         } else {
             return; //timeSlot infos can be directly see in the calendar
         }
-    },
+    }
 
-    timeSlots: function () {
-        var task = this;
+    timeSlots() {
+        var task = this.currentData();
         var timeSlots = task.timeSlots;
         if (CurrentAssignmentType.get() === AssignmentType.USERTOTASK) {
             var result = [];
@@ -134,10 +271,10 @@ Template.assignmentTasksList.helpers({
         } else {
             return []; //timeSlot infos can be directly see in the calendar
         }
-    },
+    }
 
-    peopleNeeded: function () {
-        var peopleNeeded = this.peopleNeeded;
+    peopleNeeded() {
+        var peopleNeeded = this.currentData().peopleNeeded;
 
         if (CurrentAssignmentType.get() === AssignmentType.USERTOTASK) {
             var result = [];
@@ -189,135 +326,27 @@ Template.assignmentTasksList.helpers({
 
 
         var skills = timeSlot.skills;
-    },
+    }
 
-    neededTeamOptions : function(){
+    neededTeamOptions() {
         return {
             chooseLabel: "Choose a needed team",
         };
-    },
+    }
 
-    responsibleTeamOptions : function(){
+    responsibleTeamOptions() {
         return {
             chooseLabel: "Choose a responsible team",
         };
     }
 
 
-});
+}
 
-Template.assignmentTasksList.events({
-    "click .href-assignment-task": function (event) {
-        console.info("routing", "/assignment/task/" + this._id);
-        Router.go("/assignment/task/" + this._id);
-    },
-
-    "click .task": function(event){
-        SelectedTaskBreadCrumb.set(this);
-    },
-
-    "click li.peopleNeed": function (event) {
-        event.stopPropagation();
-        var currentAssignmentType = CurrentAssignmentType.get();
-        var isUnassignment = IsUnassignment.get();
-        var target = $(event.target);
-        var _idTask, _idTimeSlot;
-        if (target.hasClass("task"))
-            _idTask = target.data("_id");
-        else
-            _idTask = target.parents(".task").data("_id");
-
-        SelectedPeopleNeed.set(this);
-
-        var userId = SelectedUser.get()._id;
+AssignmentTasksList.register("AssignmentTasksList");
 
 
-        if (target.hasClass("time-slot"))
-            _idTimeSlot = target.data("_id");
-        else
-            _idTimeSlot = target.parents(".time-slot").data("_id");
-
-        switch (currentAssignmentType) {
-            case AssignmentType.USERTOTASK:
-                if (isUnassignment) {
-                    Meteor.call("removeAssignUserToTaskTimeSlot", SelectedPeopleNeed.get()._id, userId);
-                    IsUnassignment.set(false);
-                } else
-                    Meteor.call("assignUserToTaskTimeSlot", SelectedPeopleNeed.get()._id, userId, function(error, result){
-                        if(!error){
-                            //we have to reset the task list this way because once assigned, the task can't propose any peopleNeed for the user as he is
-                            //no longer available but the task can still be theoretically assigned to him because we didn't recompute the TaskFilter job done
-                            //by the event on the calendar.
-                            //we don't need to do that for the mode TASKTOUSER as the user naturally disappears from the list once assigned
-                            TaskFilter.set(noneFilter);
-
-                            //reset workflow
-                            SelectedAvailability.set(null);
-                        }
-                    });
-                break;
-            case AssignmentType.TASKTOUSER:
-                break;
-        }
 
 
-    },
 
-
-    "keyup #search_task_name": function (event) {
-        var searchInput = $("#search_task_name").val();
-
-        //desactivation de la recherche par URL
-        //console.info("routing", "/assignment/task/search/"+query);
-        //Router.go("/assignment/task/search/"+query);
-
-        if (searchInput === "") {
-            TaskIndexFilter.set(noSearchFilter);
-        } else {
-            TaskIndexFilter.set(searchInput);
-        }
-    },
-
-    "change .filter_team": function (event) {
-        var _id = $(event.target).val();
-        if (_id === "") {
-            TaskTeamFilter.set(defaultFilter);
-        } else {
-            TaskTeamFilter.set({
-                teamId: _id
-            });
-        }
-    },
-
-    "change #filter_needed_team_task": function (event) {
-        var _id = $(event.target).val();
-        if (_id === "") {
-            TaskNeededTeamFilter.set(null);
-        } else if (_id === "noNeededTeam") {
-            TaskNeededTeamFilter.set("noNeededTeam");
-        }
-        else {
-            TaskNeededTeamFilter.set(_id);
-        }
-    },
-
-    "change #filter_skills_task": function (event) {
-        var _ids = $(event.target).val();
-        if (!_ids) {
-            TaskSkillsFilter.set(null);
-        } else if (_ids[0] === "noSkills") {
-            TaskSkillsFilter.set([]);
-        } else {
-            TaskSkillsFilter.set(_ids);
-        }
-    },
-
-    "change #display-assigned-task-checkbox": function(event){
-        DisplayAssignedTask.set($($(event.target)[0]).is(':checked'));
-    }
-});
-
-Template.assignmentTasksList.rendered = function(){
-    $('#assignment-task-list-collapsible').collapsible({});
-};
 
