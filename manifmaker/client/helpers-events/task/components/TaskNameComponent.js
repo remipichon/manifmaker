4df103961812414dc043e86b207bc14c83b8361f
RémiPichon <pichon.remi.pr@gmail.com>
2016-06-06 19:26:17 +0200

    [RPI] update task name from task update form with 'enter' key

--- manifmaker/client/helpers-events/task/components/TaskNameComponent.js
+++ manifmaker/client/helpers-events/task/components/TaskNameComponent.js
@@ -14,13 +14,27 @@ class TaskNameComponent extends BlazeComponent{
             {
                 "input .header-limited-to-text": this.displayDoneButton,//TODO more precise selector
                 "click #done-name": this.updateName,//TODO more precise selector
-                "click #edit-name": this.focusName,//TODO more precise selector
+                "click #edit-name": this.focusName,//TODO more precise selector,
+                "keydown #edit-task-name-content": this.enterKeydown
             }
         ];
     }
 
     nameIsEditing() {
         return this.nameIsEditingReactive.get();
+    }
+
+    enterKeydown(e){
+        if (e.which == 13 && e.shiftKey == false) {
+            //Prevent insertion of a return
+            //You could do other things here, for example
+            //focus on the next field
+            this.updateName();
+            e.preventDefault();
+            e.stopPropagation();
+            this.$("#edit-task-name-content").blur();
+            return false;
+        }
     }
 
     displayDoneButton() {
