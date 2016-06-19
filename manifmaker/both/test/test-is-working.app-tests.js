3d9dc429dc1aab8e124de8a3b747591adfd2af71
RémiPichon <pichon.remi.pr@gmail.com>
2016-06-19 16:33:55 +0200

    ca marche plus

--- manifmaker/both/test/test-is-working.app-tests.js
+++ manifmaker/both/test/test-is-working.app-tests.js
@@ -10,9 +10,27 @@ import {assert} from 'meteor/practicalmeteor:chai';
 describe('Test on client and server', () => {
     describe('is working', () => {
         it('test is working', () => {
-            assert.equal(true, false);
+            assert.equal(true, true);
         });
     });
 });
 
+if(Meteor.isServer){
+    describe('Test on  server !!', () => {
+        describe('is working', () => {
+            it('test is working', () => {
+                assert.equal(true, true);
+            });
+        });
+    });
+}
+if(Meteor.isClient){
+    describe('Test on clidnt !!', () => {
+        describe('is working', () => {
+            it('test is working', () => {
+                assert.equal(true, true);
+            });
+        });
+    });
+}
 
