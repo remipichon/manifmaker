019c0ca34e4832c229e7e6af9fb575defbc32562
RémiPichon <pichon.remi.pr@gmail.com>
2016-07-01 00:21:10 +0200

    #194

--- manifmaker/client/helpers-events/activity/ActivityListComponent.js
+++ manifmaker/client/helpers-events/activity/ActivityListComponent.js
@@ -2,6 +2,41 @@ class ActivityListComponent extends BlazeComponent {
     template() {
         return "activityListComponent";
     }
+
+
+    activitiesList() {
+        var fields = [
+            {
+                key: 'name',
+                label: 'Task name',
+                fnAdjustColumnSizing: true
+            },
+            {
+                key: 'teamId',
+                label: 'Team',
+                fnAdjustColumnSizing: true,
+                searchable: false,
+                fn: function (teamId, Task) {
+                    return Teams.findOne(teamId).name;
+                }
+            },
+            {
+                label: 'Actions',
+                searchable: false,
+                tmpl: Template.activityButtons,
+                fnAdjustColumnSizing: true
+            }
+        ];
+
+        return {
+            collection: Activities,
+            rowsPerPage: 10,
+            showFilter: false,
+            showRowCount: true,
+            fields: fields
+        }
+    }
+
 }
 
 ActivityListComponent.register("ActivityListComponent");
