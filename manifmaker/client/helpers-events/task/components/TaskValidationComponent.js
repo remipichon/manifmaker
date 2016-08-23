63629efa8e3a970d39f33684ffd9b125c2971e59
Lucas OH <luky.herbin@free.fr>
2016-08-24 01:21:18 +0200

    [LOH] Validation design - and adding collapse class in TasksList

--- manifmaker/client/helpers-events/task/components/TaskValidationComponent.js
+++ manifmaker/client/helpers-events/task/components/TaskValidationComponent.js
@@ -1,23 +1,40 @@
 class TaskValidationComponent extends BlazeComponent{
+    events() {
+        return [{
+            "click .toggle-equipment-validation-comments-list": this.switchEquipmentListDeveloped,
+            "click .toggle-time-slots-validation-comments-list": this.switchTimeSlotsListDeveloped,
+        }];
+    }
+
     template(){
         return "taskValidation"
     }
 
-    onRendered() {
-        this.$('.collapse').collapse({toggle: false});
-
-        this.$('.collapse').on('shown.bs.collapse	', _.bind(function () {
-            var glyphicon = this.$("[data-target=#" + $(arguments[0].target).attr("id") + "] span.glyphicon");
-            glyphicon.removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
-        }, this));
-
-        this.$('.collapse').on('hidden.bs.collapse', _.bind(function () {
-            var glyphicon = this.$("[data-target=#" + $(arguments[0].target).attr("id") + "] span.glyphicon");
-            glyphicon.removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
-        }, this));
-
+    onCreated() {
+        this.EquipmentCommentsListDeveloped = new ReactiveVar(false);
+        this.TimeSlotsCommentsListDeveloped = new ReactiveVar(false);
     }
 
+    /**
+     * this var is used to determine which icon should be displayed (to expand/collapse). The collapsing itself in made in bootstrap
+     */
+    isEquipmentCommentsListDeveloped(){
+        return this.EquipmentCommentsListDeveloped.get();
+    }
+
+    switchEquipmentListDeveloped(event){
+        this.EquipmentCommentsListDeveloped.set(!this.EquipmentCommentsListDeveloped.get());
+    }
+    /**
+     * this var is used to determine which icon should be displayed (to expand/collapse). The collapsing itself in made in bootstrap
+     */
+    isTimeSlotsCommentsListDeveloped(){
+        return this.TimeSlotsCommentsListDeveloped.get();
+    }
+
+    switchTimeSlotsListDeveloped(event){
+        this.TimeSlotsCommentsListDeveloped.set(!this.TimeSlotsCommentsListDeveloped.get());
+    }
 
     displayTextArea(validationType, state) {
         if (!Roles.userIsInRole(Meteor.userId(), RolesEnum[validationType]) &&
@@ -25,6 +42,8 @@ class TaskValidationComponent extends BlazeComponent{
             return false;
         return true;
     }
+
+
 }
 
 TaskValidationComponent.register("TaskValidationComponent");
