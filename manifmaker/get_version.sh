2f575233f28eb19339d0386578111c5b3cbd77c9
RémiPichon <pichon.remi.pr@gmail.com>
2016-09-23 01:13:02 +0200

    [RPI] deploy (travis) : fix version with space a the beginning

--- manifmaker/get_version.sh
+++ manifmaker/get_version.sh
@@ -1,1 +1,3 @@
-export MANIFMAKER_VERSION=$(cat manifmaker/package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g')
+MANIFMAKER_VERSION=$(cat manifmaker/package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g')
+MANIFMAKER_VERSION=${MANIFMAKER_VERSION:1}
+export MANIFMAKER_VERSION
