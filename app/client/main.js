93e5cebd2948fd047dc0b6cdc27ad8da8db98750
RémiPichon <pichon.remi.pr@gmail.com>
2016-09-28 23:16:38 +0200

    [RPI] feature (alert) : install s-alert

--- app/client/main.js
+++ app/client/main.js
@@ -75,5 +75,21 @@ Meteor.startup(function () {
             }
         });
     });
+
+     sAlert.config({
+        effect: 'stackslide',
+        position: 'top',
+        timeout: 2500,
+        html: false,
+        onRouteClose: true,
+        stack: {
+            spacing: 10, // in px
+            limit: 3 // when fourth alert appears all previous ones are cleared
+        },
+        offset: 0, // in px - will be added to first alert (bottom or top - depends of the position in config)
+        beep: false,
+        onClose: _.noop 
+    });
+
 });
 
