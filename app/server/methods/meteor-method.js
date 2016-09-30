b65ad5d6e3e795e9f9cc1bb222408bd21746ecde
RémiPichon <pichon.remi.pr@gmail.com>
2016-09-30 15:02:33 +0200

    [RPI] test (assignment) : nominal case for assignment and remove assignment

--- app/server/methods/meteor-method.js
+++ app/server/methods/meteor-method.js
@@ -5,7 +5,11 @@ import {SecurityServiceServer} from "../service/SecurityServiceServer"
 Meteor.methods({
      injectData: function(){
          SecurityServiceServer.isItProd("inject data from front");
+         Meteor.isStartingUp = true;
+         InjectDataServerService.deleteAll();
+         InjectDataServerService.initAccessRightData();
          InjectDataServerService.injectAllData();
-    }
+         Meteor.isStartingUp = false;
+     }
 });
 
