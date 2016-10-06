4e0d2bd497914ba76c810e42569f198ac29f0e46
RémiPichon <pichon.remi.pr@gmail.com>
2016-10-06 17:44:25 +0200

    [RPI] feat (user) : user list (filter by team doesn't work)

---
+++ app/client/helpers-events/user/UserListComponent.js
@@ -0,0 +1,83 @@
+import {TeamService} from "../../../both/service/TeamService"
+
+class UserListComponent extends BlazeComponent {
+    template() {
+        return "userListComponent";
+    }
+
+    events() {
+        return [{
+            "keyup #search_user_name": this.filterName,
+        }];
+    }
+
+    filterTeam(error, docModified, newOption) {
+        return _.bind(function (error, docModifier, newOption) {
+            var _id = newOption;
+            this.userListTeamFilter.set({teams:_id});
+        },this);
+    }
+
+    optionQueryTeamsWithoutAlreadyAssigned(){
+        return TeamService.optionQueryTeamsWithoutAlreadyAssigned();
+    }
+
+    filterName(event) {
+        event.preventDefault();
+        var _id = $(event.target).val();
+        this.userListNameFilter.set(_id);
+    }
+
+    onCreated() {
+        this.userListTeamFilter = new ReactiveTable.Filter("user-list-team-filter", ["teams"]);
+        this.userListNameFilter = new ReactiveTable.Filter('search-user-name-filter', ['name']);
+
+    }
+
+
+    usersList() {
+        var fields = [
+            {
+                key: 'name',
+                label: 'Name',
+                cellClass: 'col-sm-3',
+                headerClass: 'col-sm-3',
+                fnAdjustColumnSizing: true
+            },
+            {
+                key: 'teams',
+                label: 'Team',
+                cellClass: 'col-sm-2',
+                headerClass: 'col-sm-2',
+                fnAdjustColumnSizing: true,
+                fn: function (teams, Task) {
+                    return Teams.findOne(teams[0]).name; //user.teams : on n'utilsie que la premiere ici
+                }
+            }
+        ];
+
+        fields.push({
+            label: 'Actions',
+            cellClass: 'col-sm-2 text-center',
+            headerClass: 'col-sm-2 text-center',
+            sortable: false,
+            searchable: false, //TODO doesn't work (try with a teamId)
+            tmpl: Template.userButtons,
+            fnAdjustColumnSizing: true
+        });
+
+        return {
+            collection: Users,
+            rowsPerPage: 10,
+            showFilter: false,
+            showRowCount: true,
+            fields: fields,
+            filters: [
+                'user-list-team-filter',
+                'search-user-name-filter'
+            ]
+        }
+    }
+}
+
+UserListComponent.register("UserListComponent");
