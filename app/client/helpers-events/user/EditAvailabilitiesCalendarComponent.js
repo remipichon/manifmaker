d6419f5024cba7165298c09a278e6b949218c01b
RémiPichon <pichon.remi.pr@gmail.com>
2016-10-17 20:25:41 +0200

    [RPI] feat (user) : add availabilities from calendar (click then click)

--- app/client/helpers-events/user/EditAvailabilitiesCalendarComponent.js
+++ app/client/helpers-events/user/EditAvailabilitiesCalendarComponent.js
@@ -8,6 +8,41 @@ class EditAvailabilitiesCalendarComponent extends BaseCalendarComponent {
      this.data().parentInstance
 
      */
+
+    creanOnClick() {
+        //to implement
+        console.log("creanOnClick");
+    }
+
+    quartHeureOnClick(event) {
+        //to implement
+        var date = new moment($(event.target).parent().attr("hours"));
+
+        if(!this.startDate)
+            this.startDate = date;
+        else {
+            var endDate = date;
+            console.log("new availabilties ",this.startDate.toDate(),endDate.toDate());
+            var user = this.parentComponent().parentComponent().data();
+            var availability = {
+                start: this.startDate.toDate(),
+                end: endDate.toDate()
+            };
+            Users.update(user._id,{
+                $push : {
+                    availabilities: availability
+                }
+            });
+            this.startDate = null;
+        }
+
+    }
+
+    constructor() {
+        super();
+
+        this.startDate = null;
+    }
 }
 
 EditAvailabilitiesCalendarComponent.register("EditAvailabilitiesCalendarComponent");
