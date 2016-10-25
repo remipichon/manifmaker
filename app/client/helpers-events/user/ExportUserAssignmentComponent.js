d2c8306accbb3f80d6a99c800e559218752ea5e2
RémiPichon <pichon.remi.pr@gmail.com>
2016-10-25 19:46:49 +0200

    [LMO] feat (user export) : list assignment with data (without design)

--- app/client/helpers-events/user/ExportUserAssignmentComponent.js
+++ app/client/helpers-events/user/ExportUserAssignmentComponent.js
@@ -1,3 +1,5 @@
+import {TimeSlotService} from "../../../both/service/TimeSlotService"
+
 class ExportUserAssignmentComponent extends BlazeComponent {
 
     constructor() {
@@ -6,6 +8,25 @@ class ExportUserAssignmentComponent extends BlazeComponent {
 
     template() {
         return "exportUserAssignmentComponent";
+    }
+
+    sortedAssignments(){
+        return _.sortBy(Assignments.find({userId: this.data()._id}).fetch(), function(assigment){
+           return TimeSlotService.getTimeSlot(Tasks.findOne(assigment.taskId), assigment.timeSlotId).start
+        });
+    }
+
+    task(){
+        return Tasks.findOne(this.currentData().taskId)
+    }
+
+    timeSlot(){
+        return TimeSlotService.getTimeSlot(this.task(), this.currentData().timeSlotId)
+    }
+
+    displayAssignedUser(userId){
+        var user = Users.findOne(userId);
+        return user.name + "(066666)"
     }
 
 }
