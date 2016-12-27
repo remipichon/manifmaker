935539cb80d06a96ae66dab3e8cde2919c7da6b5
RémiPichon <pichon.remi.pr@gmail.com>
2016-12-28 00:30:47 +0100

    [RPI] evol (activity) : add role readOnly check on access passes

--- app/client/helpers-events/activity/components/ActivityAccessPassesComponent.js
+++ app/client/helpers-events/activity/components/ActivityAccessPassesComponent.js
@@ -1,7 +1,22 @@
+import {ValidationService} from "../../../../both/service/ValidationService"
+
 class ActivityAccessPassesComponent extends BlazeComponent{
 
     template(){
         return "activityAccessPasses"
+    }
+
+    constructor(isReadOnly){
+        super();
+        this.isReadOnly = isReadOnly;
+    }
+
+    isAccessPassesUpdateAllowed() {
+        return ValidationService.isUpdateAllowed(this.data().parentInstance.data().accessPassValidation.currentState);
+    }
+
+    isEquipmentsReadOnly() {
+        return this.isReadOnly || !this.isAccessPassesUpdateAllowed();
     }
 }
 
