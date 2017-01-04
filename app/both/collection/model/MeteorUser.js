0ea6b06099046efb863ee80655b905118eee6edf
Rémi <pichon.remi.pr@gmail.com>
2017-01-04 21:56:49 +0100

    [RPI] fix (activity) : add limitToTeam field in create activity form

--- app/both/collection/model/MeteorUser.js
+++ app/both/collection/model/MeteorUser.js
@@ -395,7 +395,8 @@ Schemas.User = new SimpleSchema({
         // For accounts-password, either emails or username is required, but not both. It is OK to make this
         // optional here because the accounts-password package does its own validation.
         // Third-party login packages may not require either. Adjust this schema as necessary for your usage.
-        optional: true
+        optional: true,
+        unique: true
     },
     emails: {
         type: Array,
