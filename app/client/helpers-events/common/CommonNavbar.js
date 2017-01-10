84a64258631cb65e6bbf0f88bf3f69d90d705665
Rémi <pichon.remi.pr@gmail.com>
2017-01-10 15:51:26 +0100

    [RPI] feat (nabber) : add clock to nav bar

--- app/client/helpers-events/common/CommonNavbar.js
+++ app/client/helpers-events/common/CommonNavbar.js
@@ -3,8 +3,14 @@ import {AssignmentReactiveVars} from "../../../client/helpers-events/assignment/
 
 class CommonNavbar extends BlazeComponent {
 
+    constructor() {
+        super();
+        this.self = this;
+        this.timedUpdate();
+    }
+
     lastUpdateDate() {
-        var updateInfo =  UpdateInfo.find({}).fetch();
+        var updateInfo = UpdateInfo.find({}).fetch();
         var lastInfo = updateInfo[updateInfo.length - 1];
         var lastDate = new moment(lastInfo.date);
         var now = new moment();
@@ -21,32 +27,46 @@ class CommonNavbar extends BlazeComponent {
         var rotationStep = iconTurn * 360 / numberOfSteps
         var rot = 0;
 
-        var highlightEffect = setInterval(_.bind(function(){
+        var highlightEffect = setInterval(_.bind(function () {
             opacity = opacity - step;
             rot += rotationStep;
-            this.$(".lastUpdate").css("background",`rgba(57, 241, 44, ${opacity})`);
+            this.$(".lastUpdate").css("background", `rgba(57, 241, 44, ${opacity})`);
 
             var icon = this.$(".lastUpdate .mdi");
 
-            icon.css("-webkit-transform",`rotate(${rot}deg)`);
-            icon.css("-moz-transform",`rotate(${rot}deg)`);
-            icon.css("-ms-transform",`rotate(${rot}deg)`);
-            icon.css("-o-transform",`rotate(${rot}deg)`);
-            icon.css("transform",`rotate(${rot}deg)`);
+            icon.css("-webkit-transform", `rotate(${rot}deg)`);
+            icon.css("-moz-transform", `rotate(${rot}deg)`);
+            icon.css("-ms-transform", `rotate(${rot}deg)`);
+            icon.css("-o-transform", `rotate(${rot}deg)`);
+            icon.css("transform", `rotate(${rot}deg)`);
 
 
-            if(opacity <= 0){
+            if (opacity <= 0) {
                 clearInterval(highlightEffect);
             }
 
-        },this), delay);
-
+        }, this), delay);
 
 
         return lastDate.toDate();
     }
 
 
+    updateClock() {
+        var now = moment(),
+            second = now.seconds() * 6,
+            minute = now.minutes() * 6 + second / 60,
+            hour = ((now.hours() % 12) / 12) * 360 + 90 + minute / 12;
+
+        self.$('#hour').css("transform", "rotate(" + hour + "deg)");
+        self.$('#minute').css("transform", "rotate(" + minute + "deg)");
+        self.$('#second').css("transform", "rotate(" + second + "deg)");
+    }
+
+    timedUpdate() {
+        this.updateClock();
+        setTimeout(_.bind(this.timedUpdate, this), 1000);
+    }
 
 
 }
