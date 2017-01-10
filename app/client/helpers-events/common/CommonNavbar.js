50162a7596afbec668cfd0f8ac4c81ab9ee56477
Rémi <pichon.remi.pr@gmail.com>
2017-01-10 15:03:01 +0100

    [RPI] design (last update) : mid icon turn while date is highlighted

--- app/client/helpers-events/common/CommonNavbar.js
+++ app/client/helpers-events/common/CommonNavbar.js
@@ -17,10 +17,24 @@ class CommonNavbar extends BlazeComponent {
         var step = 0.01;
         var numberOfSteps = 0.6 / step;
         var delay = duration / numberOfSteps;
+        var iconTurn = 2;
+        var rotationStep = iconTurn * 360 / numberOfSteps
+        var rot = 0;
 
         var highlightEffect = setInterval(_.bind(function(){
             opacity = opacity - step;
-            this.$(".lastUpdate").css("background",`rgba(57, 241, 44, ${opacity})`)
+            rot += rotationStep;
+            this.$(".lastUpdate").css("background",`rgba(57, 241, 44, ${opacity})`);
+
+            var icon = this.$(".lastUpdate .mdi");
+
+            icon.css("-webkit-transform",`rotate(${rot}deg)`);
+            icon.css("-moz-transform",`rotate(${rot}deg)`);
+            icon.css("-ms-transform",`rotate(${rot}deg)`);
+            icon.css("-o-transform",`rotate(${rot}deg)`);
+            icon.css("transform",`rotate(${rot}deg)`);
+
+
             if(opacity <= 0){
                 clearInterval(highlightEffect);
             }
