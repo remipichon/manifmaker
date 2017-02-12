a979629ee7a7da1dc1df0bdbbaca4bf744b4e2ff
Rémi <pichon.remi.pr@gmail.com>
2017-02-12 21:31:58 +0100

    [RPI] feat (backup mongo) : ssh key and backup server ip on the run

--- production/mongodb-backup/run.sh
+++ production/mongodb-backup/run.sh
@@ -31,7 +31,7 @@ if ${BACKUP_CMD} ;then
         tar czvf /manifmaker_backup_tar/${BACKUP_NAME}.tar.gz /backup/${BACKUP_NAME}
         scp -r /manifmaker_backup_tar/${BACKUP_NAME}.tar.gz root@vps302914:/root/from_prod_backup_tar/
         export BACKUP_NAME
-        ssh root@vps302914.ovh.net "mkdir /root/from_prod_backup/${BACKUP_NAME} \
+        ssh ${BACKUP_SERVER} "mkdir /root/from_prod_backup/${BACKUP_NAME} \
         && tar xzvf /root/from_prod_backup_tar/${BACKUP_NAME}.tar.gz -C /root/from_prod_backup/${BACKUP_NAME} \
         && mv /root/from_prod_backup/${BACKUP_NAME}/root/manifmaker_backup/${BACKUP_NAME}/ /root/manifmaker_backup/prod_${BACKUP_NAME}/ \
         && rm -rf /root/from_prod_backup/${BACKUP_NAME} \ "
