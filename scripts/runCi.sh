bde8719ec7ae0e9982a117015ab3b056b9dbba90
Rémi <pichon.remi.pr@gmail.com>
2017-02-14 00:06:45 +0100

    8

--- scripts/runCi.sh
+++ scripts/runCi.sh
@@ -9,7 +9,7 @@ then
   echo "=production="
   echo "... Now deploying to Production "
   ssh root@vps302915.ovh.net "cd manifmaker; git reset --hard HEAD; git pull origin production"
-  ssh root@vps302915.ovh.net "cd manifmaker/production; docker-compose up -d manifmaker"
+  #ssh root@vps302915.ovh.net "cd manifmaker/production; docker-compose up -d manifmaker"
 fi 
 
 if [[ $BRANCH_NAME = "deploy" ]];
